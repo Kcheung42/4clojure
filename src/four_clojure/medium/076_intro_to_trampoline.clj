e084a2ec901f7261912f05cc64e4aa21d263666c
Kenneth <kcheung331@gmail.com>
2020-06-25 14:36:48 -0700

    added #76

---
+++ src/four_clojure/medium/076_intro_to_trampoline.clj
@@ -0,0 +1,35 @@
+(ns four-clojure.medium.076-intro-to-trampoline)
+
+;; -----------------------------------------------------------------------------
+;; Question
+;; -----------------------------------------------------------------------------
+;; tags:
+
+;; The trampoline function takes a function f and a variable number of
+;; parameters. Trampoline calls f with any parameters that were supplied.
+;; If f returns a function, trampoline calls that function with no arguments.
+;; This is repeated, until the return value is not a function, and then trampoline
+;; returns that non-function value. This is useful for implementing mutually
+;; recursive algorithms in a way that won't consume the stack.
+
+
+;; -----------------------------------------------------------------------------
+;; Solutions
+;; -----------------------------------------------------------------------------
+
+(def result [1 3 5 7 9 11])
+
+;; -----------------------------------------------------------------------------
+;; Tests
+;; -----------------------------------------------------------------------------
+
+(every? true?
+        (vector
+          (= result
+             (letfn
+                 [(foo [x y] #(bar (conj x y) y))
+                  (bar [x y] (if (> (last x) 10)
+                               x
+                               #(foo x (+ 2 y))))]
+               (trampoline foo [] 1)))
+          ))
