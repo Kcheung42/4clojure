455a2b20ad494279506daa37f5faa67834181e4a
Kenneth <kcheung331@gmail.com>
2020-06-25 20:09:35 -0700

    moved #50

--- src/four_clojure/medium/050_split_by_type.clj
+++ src/four_clojure/medium/050_split_by_type.clj
@@ -20,26 +20,40 @@
    h[type] => [v1, v2 ...]
    )
 
-(defn f
+(comment
+  (defn split-by-type
+    [coll]
+    (vals
+      (reduce (fn [acc, x]
+                (let [ttype (type x)]
+                  (assoc acc ttype (conj (get acc ttype []) x))))
+              {}
+              coll))))
+
+;; why does this overflow when coll is (range 4000)
+(comment
+  (defn split-by-type
+    [coll]
+    (vals
+      (apply merge-with into (map (fn [x] {(type x) [x]}) coll)))))
+
+
+;; more concise
+(defn split-by-type
   [coll]
-  (vals
-    (reduce (fn [acc, x]
-              (let [ttype (type x)]
-                (assoc acc ttype (conj (get acc ttype []) x))))
-      {}
-      coll)))
+  (vals (group-by type coll)))
 
 ;; -----------------------------------------------------------------------------
 ;; Tests
 ;; -----------------------------------------------------------------------------
 
 (every? true?
-  (vector
-    (= (set (f [1 :a 2 :b 3 :c]))
-      #{[1 2 3] [:a :b :c]})
+        (vector
+          (= (set (split-by-type [1 :a 2 :b 3 :c]))
+             #{[1 2 3] [:a :b :c]})
 
-    (= (set (f [:a "foo"  "bar" :b]))
-      #{[:a :b] ["foo" "bar"]})
+          (= (set (split-by-type [:a "foo"  "bar" :b]))
+             #{[:a :b] ["foo" "bar"]})
 
-    (= (set (f [[1 2] :a [3 4] 5 6 :b]))
-      #{[[1 2] [3 4]] [:a :b] [5 6]})))
+          (= (set (split-by-type [[1 2] :a [3 4] 5 6 :b]))
+             #{[[1 2] [3 4]] [:a :b] [5 6]})))
