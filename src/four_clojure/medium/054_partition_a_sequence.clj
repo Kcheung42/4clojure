bd54f2503a961afd2f9c5b1e282a9d2ade09f13b
Kenneth <kcheung331@gmail.com>
2020-07-06 09:55:47 -0700

    notes for #54

--- src/four_clojure/medium/054_partition_a_sequence.clj
+++ src/four_clojure/medium/054_partition_a_sequence.clj
@@ -26,6 +26,41 @@
   (when (and (seq coll) (>= (count coll) n))
     (cons (take n coll) (partition- n (drop n coll) ))))
 
+(defn partition-
+  [n coll]
+  (when-let [s (seq coll)]
+    (when (<= n (count s))
+      (cons (take n s) (partition- n (drop n s))))))
+
+(comment ;;Source
+  (defn partition
+    "Returns a lazy sequence of lists of n items each, at offsets step
+  apart. If step is not supplied, defaults to n, i.e. the partitions
+  do not overlap. If a pad collection is supplied, use its elements as
+  necessary to complete last partition upto n items. In case there are
+  not enough padding elements, return a partition with less than n items."
+    {:added  "1.0"
+     :static true}
+    ([n coll]
+     (partition n n coll))
+
+    ([n step coll]
+     (lazy-seq
+       (when-let [s (seq coll)]
+         (let [p (doall (take n s))] ;;#2
+           (when (= n (count p))
+             (cons p (partition n step (nthrest s step)))))))) ;;#1
+
+    #_([n step pad coll]
+       (lazy-seq
+         (when-let [s (seq coll)]
+           (let [p (doall (take n s))]
+             (if (= n (count p))
+               (cons p (partition n step pad (nthrest s step)))
+               (list (take n (concat p pad))))))))))
+
+;; #1 Why use `nthrest` instead of `drop`?
+;; #2 Why `doall` ?
 
 ;; -----------------------------------------------------------------------------
 ;; Tests
