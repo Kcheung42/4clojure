fe395a9cd85efa7255d3c4bd4f7b6a261aef1cb3
Kenneth <kcheung331@gmail.com>
2020-07-06 09:20:23 -0700

    Notes for distinct

--- src/four_clojure/medium/056_find_distinct_items.clj
+++ src/four_clojure/medium/056_find_distinct_items.clj
@@ -46,6 +46,58 @@
              (conj seen f)
              (if (seen f) rslt (conj rslt f))))))
 
+(avg (repeatedly 1000 #(bench (remove-dup- (repeat 1000000 1)))))
+;; => Elapsed time: 814.123751 msecs
+
+(defn distinct-source
+  [coll]
+  (let [step (fn step [xs seen]
+               (lazy-seq ;;#3
+                 ((fn [[f :as xs] seen]    ;;#1
+                    (when-let [s (seq xs)]
+                      (if (contains? seen f)
+                        (recur (rest s) seen) ;;#2
+                        (cons f (step (rest s) (conj seen f)))))) ;;#4
+                  xs seen)))]
+    (step coll #{})))
+;; (time (time (remove-dup- (repeat 1000000 1))))
+;; => Elapsed time: 0.069488 msecs
+
+(defn distinct-
+  ([coll]
+   (distinct- coll #{}))
+
+  ([coll seen]
+   (lazy-seq
+     ((fn [[f :as xs] seen]
+        (when-let [s (seq xs)]
+          (if (contains? seen f)
+            (recur (rest s) seen)
+            (cons f (distinct- (rest s) (conj seen f))))))
+      coll seen))))
+
+;;#1 naming with xs when destructuring a coll
+;;#2 note this uses recur and not step
+;;#3 base case calls (lazy-seq nil) => returns (). This allows for cons to work
+;;    (cons f (lazy-seq nil)) => f
+;;#4 pattern (cons x (recursive call))
+
+(defmacro bench
+  "Times the execution of forms, discarding their output and returning
+  a long in nanoseconds."
+  ([& forms]
+   `(let [start# (System/nanoTime)]
+      ~@forms
+
+      (- (System/nanoTime) start#))))
+
+(defn avg
+  [coll]
+  (float (/ (reduce + coll) (count coll))))
+
+(avg (repeatedly 1000 #(bench (distinct (repeat 1000000 1)))))  ;;=> 927.352
+(avg (repeatedly 1000 #(bench (distinct- (repeat 1000000 1))))) ;;=> 867.697
+
 ;; -----------------------------------------------------------------------------
 ;; Tests
 ;; -----------------------------------------------------------------------------
